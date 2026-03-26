b16407f489af05d8e1c21afb2828471b94e9b3be
Volker Süß <volker.suess@gmail.com>
2026-03-26 18:38:28 +0100

    KassenbuchService im Services-Package exportiert

--- vtb_verein/app/services/__init__.py
+++ vtb_verein/app/services/__init__.py
@@ -3,3 +3,11 @@ Service Layer Package
 
 Enthält Business-Logik und orchestriert Datenbank-Operationen.
 """
+
+from app.services.kassenbuch_service import KassenbuchService, BuchungGesperrtError, NegativerBestandError
+
+__all__ = [
+    "KassenbuchService",
+    "BuchungGesperrtError",
+    "NegativerBestandError",
+]
